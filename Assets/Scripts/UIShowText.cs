eae80d4b0ca37b01aa87c4e92943b642fd42657a
Ke Jing <kjing1@uci.edu>
2020-02-08 20:15:44 -0800

    update scene

---
+++ Assets/Scripts/UIShowText.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIShowText : MonoBehaviour
+{
+    Text text;
+    public string content;
+    // Start is called before the first frame update
+    void Start()
+    {
+        text = GetComponent<Text>();
+    }
+
+
+    private void OnEnable()
+    {
+        text.text = content;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
