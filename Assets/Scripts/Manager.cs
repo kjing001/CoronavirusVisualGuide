6f236658f6190b739def61542cdcdef3fc867e19
Ke Jing <kjing1@uci.edu>
2020-02-10 22:47:05 -0800

    scene2 animal walking and window masks

--- Assets/Scripts/Manager.cs
+++ Assets/Scripts/Manager.cs
@@ -4,6 +4,16 @@ using UnityEngine;
 
 public class Manager : MonoBehaviour
 {
+    public static Manager instance;
+
+    public Transform animalArea;
+    public GameObject[] animalPrefabs;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +23,25 @@ public class Manager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            SpawnRandomAnimal();
+        }
     }
+
+    public Vector2 GetRandomPosition()
+    {
+        float randX = Random.Range(animalArea.position.x - animalArea.localScale.x/2, animalArea.position.x + animalArea.localScale.x/2);
+        float randY = Random.Range(animalArea.position.y - animalArea.localScale.y/2, animalArea.position.y + animalArea.localScale.y/2);
+        return new Vector2(randX, randY);
+    }
+
+    void SpawnRandomAnimal()
+    {
+        int randID = Random.Range(0, animalPrefabs.Length);
+
+        GameObject.Instantiate(animalPrefabs[randID], GetRandomPosition(), Quaternion.identity);
+
+    }
+
 }
