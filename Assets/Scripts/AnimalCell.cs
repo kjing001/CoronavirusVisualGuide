d64b2e1f2eba8fced884cc01918d32a400d46965
Ke Jing <kjing1@uci.edu>
2020-02-12 15:43:51 -0800

    add sprite ipointerclick

---
+++ Assets/Scripts/AnimalCell.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class AnimalCell : MonoBehaviour, IPointerClickHandler
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void OnPointerClick(PointerEventData ped)
+    {
+        //whatever happens on click
+        Debug.Log("hit");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
