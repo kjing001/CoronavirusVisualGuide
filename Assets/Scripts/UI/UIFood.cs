96ff8bf2f21d2309183d413129c06abc717b8c1b
Ke Jing <kjing1@uci.edu>
2020-02-24 18:02:56 -0800

    home scene wip
    added food interface
    added background from lika

---
+++ Assets/Scripts/UI/UIFood.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIFood : MonoBehaviour
+{
+    public Text foodCountText;
+    public Text foodInfoText;
+
+    public Button eatButton;
+
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        eatButton.onClick.AddListener(OnEat);
+    }
+
+    private void OnEat()
+    {
+        throw new NotImplementedException();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/UI/UIHome.cs
+++ Assets/Scripts/UI/UIHome.cs
@@ -1,19 +1,29 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class UIHome : MonoBehaviour
 {
+    public GameObject foodPanel;
+
     public Button newsButton;
-    public Button eatButton;
-    public Button walletButton;
+    public Button foodButton;
     public Button goOutButton;
+
+    public 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        foodPanel.SetActive(false);
+        foodButton.onClick.AddListener(OnFood);
+    }
+
+    private void OnFood()
+    {
+        foodPanel.SetActive(!foodPanel.activeSelf);
     }
 
     // Update is called once per frame
