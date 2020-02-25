9433a890e6dc2b36f41921a9441308cfcb038c06
Ke Jing <kjing1@uci.edu>
2020-02-24 16:12:55 -0800

    add player

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public float money;
+    public float immune;
+    public float mentalHealth;
+
     // Start is called before the first frame update
     void Start()
     {
