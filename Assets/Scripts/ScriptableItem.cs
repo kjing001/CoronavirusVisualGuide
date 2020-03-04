495c3a7bc390bef6014adb36b85b5e8a37339436
Ke Jing <kjing1@uci.edu>
2020-03-04 01:57:41 -0800

    scriptable object wip

--- Assets/Scripts/ScriptableItem.cs
+++ Assets/Scripts/ScriptableItem.cs
@@ -1,37 +1,18 @@
 ﻿// Saves the item info in a ScriptableObject that can be used ingame by
 // referencing it from a MonoBehaviour. It only stores an item's static data.
-//
-// We also add each one to a dictionary automatically, so that all of them can
-// be found by name without having to put them all in a database. Note that we
-// have to put them all into the Resources folder and use Resources.LoadAll to
-// load them. This is important because some items may not be referenced by any
-// entity ingame (e.g. when a special event item isn't dropped anymore after the
-// event). But all items should still be loadable from the database, even if
-// they are not referenced by anyone anymore. So we have to use Resources.Load.
-// (before we added them to the dict in OnEnable, but that's only called for
-//  those that are referenced in the game. All others will be ignored be Unity.)
-//
-// An Item can be created by right clicking the Resources folder and selecting
-// Create -> uMMORPG Item. Existing items can be found in the Resources folder.
-//
-// Note: this class is not abstract so we can create 'useless' items like recipe
-// ingredients, etc.
+
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using UnityEngine;
 
-[CreateAssetMenu(menuName="uMMORPG Item/General", order=999)]
+[CreateAssetMenu(menuName = "Item/General", order = 999)]
 public partial class ScriptableItem : ScriptableObjectNonAlloc
 {
     [Header("Base Stats")]
     public int maxStack;
-    public long buyPrice;
-    public long sellPrice;
-    public long itemMallPrice;
-    public bool sellable;
-    public bool tradable;
+    public float price;
     public bool destroyable;
     [SerializeField, TextArea(1, 30)] protected string toolTip; // not public, use ToolTip()
     public Sprite image;
@@ -61,10 +42,6 @@ public partial class ScriptableItem : ScriptableObjectNonAlloc
         StringBuilder tip = new StringBuilder(toolTip);
         tip.Replace("{NAME}", name);
         tip.Replace("{DESTROYABLE}", (destroyable ? "Yes" : "No"));
-        tip.Replace("{SELLABLE}", (sellable ? "Yes" : "No"));
-        tip.Replace("{TRADABLE}", (tradable ? "Yes" : "No"));
-        tip.Replace("{BUYPRICE}", buyPrice.ToString());
-        tip.Replace("{SELLPRICE}", sellPrice.ToString());
         return tip.ToString();
     }
 
@@ -101,13 +78,6 @@ public partial class ScriptableItem : ScriptableObjectNonAlloc
         }
     }
 
-    // validation //////////////////////////////////////////////////////////////
-    void OnValidate()
-    {
-        // make sure that the sell price <= buy price to avoid exploitation
-        // (people should never buy an item for 1 gold and sell it for 2 gold)
-        sellPrice = Math.Min(sellPrice, buyPrice);
-    }
 }
 
 // ScriptableItem + Amount is useful for default items (e.g. spawn with 10 potions)
